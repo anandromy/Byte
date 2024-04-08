fa7cb1f6c8fbc22a4f33c5525f5f280c245acb6b
anandromy <23f3003270@ds.study.iitm.ac.in>
2024-04-08 20:59:03 +0530

    exit button in navbar-routes redirect to '/'

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -16,10 +16,12 @@ export const NavbarRoutes = () => {
         <div className="flex gap-x-2 ml-auto">
             {
                 isTeacherPage ?
-                <Button size="sm" variant="ghost">
-                    <LogOut className="h-4 w-4 mr-2"/>
-                    Exit
-                </Button>
+                <Link href="/">
+                    <Button size="sm" variant="ghost">
+                        <LogOut className="h-4 w-4 mr-2"/>
+                        Exit
+                    </Button>
+                </Link>
                 :
                 <Link href="/teacher/courses">
                     <Button size="sm" variant="ghost">
