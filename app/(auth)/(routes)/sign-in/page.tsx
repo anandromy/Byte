c0840e2c41d826b314be18402c21845b59f1e121
anandromy <23f3003270@ds.study.iitm.ac.in>
2024-04-08 10:23:40 +0530

    Initialized basic clerk implementation + installed shadcn button ui

---
+++ app/(auth)/(routes)/sign-in/page.tsx
@@ -0,0 +1,9 @@
+const SigninPage = () => {
+    return(
+        <div>
+            This is an unprotected route
+        </div>
+    )
+}
+
+export default SigninPage
---
+++ app/(dashboard)/(routes)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <p className="font-bold text-3xl text-red-500">Hello</p>
+      <Button variant="outline">Click me</Button>
+    </div>
+  );
+}
