dc60a52879e5b1d7a802aeb0849adf4a87cd8ee9
anandromy <23f3003270@ds.study.iitm.ac.in>
2024-04-09 17:39:13 +0530

    UI: IconBadge

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,5 +1,7 @@
+import { IconBadge } from "@/components/icon-badge";
 import { db } from "@/lib/db"
 import { auth } from "@clerk/nextjs";
+import { LayoutDashboard } from "lucide-react";
 import { redirect } from "next/navigation";
 
 
@@ -40,6 +42,16 @@ const CourseIdPage = async ({ params }: { params: { courseId: string }}) => {
                     <span className="text-sm text-slate-700">
                         Complete all fields {completionText}
                     </span>
+                </div>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+                <div>
+                    <div className="flex items-center gap-x-2">
+                        <IconBadge icon={LayoutDashboard} />
+                        <h2 className="text-xl">
+                            Customize your course
+                        </h2>
+                    </div>
                 </div>
             </div>
         </div>
