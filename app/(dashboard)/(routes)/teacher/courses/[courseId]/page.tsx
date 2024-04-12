4d9803a735e6849c4ccfdada05678f820ff099b4
anandromy <23f3003270@ds.study.iitm.ac.in>
2024-04-12 16:41:24 +0530

    feat: Adding attachments for the course, new api route for adding attachments

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,13 +1,14 @@
 import { IconBadge } from "@/components/icon-badge";
 import { db } from "@/lib/db"
 import { auth } from "@clerk/nextjs";
-import { CircleDollarSign, LayoutDashboard, ListChecks } from "lucide-react";
+import { CircleDollarSign, File, LayoutDashboard, ListChecks } from "lucide-react";
 import { redirect } from "next/navigation";
 import { TitleForm } from "./_components/title-form";
 import { DescriptionForm } from "./_components/description-form";
 import { ImageForm } from "./_components/image-form";
 import { Categoryform } from "./_components/category-form";
 import { PriceForm } from "./_components/price-form";
+import { AttachmentForm } from "./_components/attachment-form";
 
 
 const CourseIdPage = async ({ params }: { params: { courseId: string }}) => {
@@ -18,6 +19,13 @@ const CourseIdPage = async ({ params }: { params: { courseId: string }}) => {
     const course = await db.course.findUnique({
         where: {
             id: params.courseId
+        },
+        include: {
+            attachments: {
+                orderBy: {
+                    createdAt: "desc"
+                }
+            }
         }
     });
     if(!course){
@@ -107,6 +115,18 @@ const CourseIdPage = async ({ params }: { params: { courseId: string }}) => {
                             initialData={course}
                             courseId={course.id}
                         />
+                    </div>
+                    <div>
+                        <div className="flex items-center gap-x-2">
+                            <IconBadge icon={File}></IconBadge>
+                            <h2 className="text-xl">
+                                Resources & Attachments
+                            </h2>
+                        </div>
+                        <AttachmentForm 
+                            initialData={course}
+                            courseId={course.id}
+                        /> 
                     </div>
                 </div>
             </div>
